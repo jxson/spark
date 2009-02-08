c4e7cfa7ae80d67a7ff48cea4eb128e09cb878de
Ian Hunter <ian@mamba.local>
2009-02-08 01:20:44 -0800

    right subdirs created now

--- install.rb
+++ install.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 
 FileUtils.mkdir_p RAILS_ROOT+'/db/spark'
 Dir.glob(File.join(RAILS_ROOT,"config/environments",'*.{rb}')).each do |env|
-  FileUtils.mkdir_p RAILS_ROOT+'/db/spark/'+env
+  FileUtils.mkdir_p RAILS_ROOT+'/db/spark/'+File.basename(env, '.*')
 end
 
 # s3_config = File.dirname(__FILE__) + '/../../../config/amazon_s3.yml'
